f3a3de769a487dabf6241a02a81614bc6663e8e7
brianpkho <brianpk.ho@mail.utoronto.ca>
2020-07-14 19:58:15 -0400

    Display highlighted bounding box image

--- object_detection.py
+++ object_detection.py
@@ -30,6 +30,8 @@ class DrawBoundingBox:
             self.ref_point.append((x, y))
             self.record.append(x)
             self.record.append(y)
+            # Save the image before writing
+            cv2.imwrite('./images/' + param[0].replace('.mpg', str(param[1]) + '.jpeg'), self.image)
             # draw a rectangle around the region of interest
             cv2.rectangle(self.image, self.ref_point[0], self.ref_point[1], (0, 255, 0), 2)
 
@@ -37,17 +39,29 @@ class DrawBoundingBox:
             self.ref_point = []
             cv2.imshow("image", self.image)
     
-    
+    def display_bounding_box(self, filename):
+        bbox = pd.read_csv('./bbox/' + filename[:-1] + '_bbox.csv')
+        # Get bounding box points
+        x1 = bbox.loc[bbox['name'] == filename + '.jpeg']['x1']
+        y1 = bbox.loc[bbox['name'] == filename + '.jpeg']['y1']
+        x2 = bbox.loc[bbox['name'] == filename + '.jpeg']['x2']
+        y2 = bbox.loc[bbox['name'] == filename + '.jpeg']['y2']
+
+        img = cv2.imread('./images/' + filename + '.jpeg')
+        cv2.rectangle(img, (x1, y1), (x2,y2), (0,255,0), 2)
+        cv2.imshow("image2", img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
     
     def run(self, filename):
-        vid = cv2.VideoCapture(self.lapse_folder + filename)
-        self.record.append(filename)
-        
-        cv2.namedWindow("image")
-        cv2.setMouseCallback("image", self.shape_selection)
-        
         try:
+            vid = cv2.VideoCapture(self.lapse_folder + filename)
+            frame = 0
+            # file name convention
+            self.record.append(filename.replace('.mpg', str(frame) + '.jpeg'))
             while vid.isOpened():
+                cv2.namedWindow("image")
+                cv2.setMouseCallback("image", self.shape_selection, [filename,frame])
                 _, self.image = vid.read()
                 clone = self.image.copy()
                 # keep looping until the 'q' key is pressed
@@ -59,14 +73,15 @@ class DrawBoundingBox:
 
                     # press 'r' to reset the window
                     if key == ord("r"):
-                        record = [filename]
+                        self.record = [filename.replace('.mpg', str(frame) + '.jpeg')]
                         self.image = clone.copy()
 
                     # Continue video
                     if key == 13 or key == -1:
                         # save to pandas dataframe
                         self.df = self.df.append([self.record], ignore_index=True)
-                        self.record = [filename]
+                        frame += 1
+                        self.record = [filename.replace('.mpg', str(frame) + '.jpeg')]
                         break
                 # Exit loop if it's q
                 if key == ord("q"):
@@ -74,7 +89,7 @@ class DrawBoundingBox:
                     
             if self.df.shape[0] > 0:
                 self.df.columns = ['name', 'x1','y1','x2','y2']
-                self.df.to_csv(self.out_folder + filename.replace('.mpg','').replace('Timelapses', 'bbox') + '_bbox.csv',index=False)
+                self.df.to_csv(self.out_folder + filename.replace('.mpg', '_bbox.csv').replace('Timelapses', 'bbox') ,index=False)
             
         except Exception as e:
             print(e)
@@ -87,3 +102,5 @@ if __name__ == '__main__':
     x = DrawBoundingBox("./Timelapses/", './bbox/')
     # Run it on 1 video
     x.run('asun_1_20200710101504.mpg')
+    frame = 0
+    x.display_bounding_box('asun_1_20200710101504' + str(frame))
